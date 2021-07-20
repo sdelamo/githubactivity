fa195a9e4923f8d5740cfd55c6d69cee533a4888
Sergio del Amo <sergio.delamo@softamo.com>
2021-07-20 09:50:33 +0200

    better presentation

--- src/main/java/com/softamo/ghactivity/cli/GhActivityCommand.java
+++ src/main/java/com/softamo/ghactivity/cli/GhActivityCommand.java
@@ -14,6 +14,8 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.Map;
+import java.util.HashMap;
 
 @Command(name = "ghactivity", description = "...",
         mixinStandardHelpOptions = true)
@@ -46,6 +48,7 @@ public class GhActivityCommand implements Runnable {
         Collections.sort(l);
         Set<LocalDate> outputDays = new HashSet<>();
         Set<String> repos = new HashSet<>();
+        Map<String, Set<Integer>> reposActivity = new HashMap<>();
         for (DayActivity activity : l) {
             if (!outputDays.contains(activity.getDay())) {
                 outputDays.add(activity.getDay());
@@ -56,12 +59,21 @@ public class GhActivityCommand implements Runnable {
                 repos.add(activity.getRepo());
                 System.out.println("## " + activity.getRepo());
             }
-            System.out.println(activity.getType() + " " + (activity.getNumber() != null ? activity.getNumber() : "") +
-                    " " +
-                            (activity.getTitle() != null ? activity.getTitle() : ""));
+
+            if (activity.getNumber() != null) {
+                if (reposActivity.containsKey(activity.getRepo()) &&
+                    reposActivity.get(activity.getRepo()).contains(activity.getNumber())) {
+                    continue;
+                }
+                System.out.println(activity.getNumber() + " " + (activity.getTitle() != null ? activity.getTitle() : ""));
+                if (reposActivity.containsKey(activity.getRepo())) {
+                    Set<Integer> numbers = reposActivity.get(activity.getRepo());
+                    numbers.add(activity.getNumber());
+                    reposActivity.put(activity.getRepo(), numbers);
+                } else {
+                    reposActivity.put(activity.getRepo(), new HashSet<>(activity.getNumber()));
+                }
+            }
         }
     }
-
-
-
 }
